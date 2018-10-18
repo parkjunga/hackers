232cf550a87efd44052e6a25e11b4ab85dc16f2a
[{웹개발2팀}] {박정아} <{ejh98712@naver.com}>
2018-10-18 14:04:08 +0900

    비밀번호찾기 기능 구현

---
+++ member/find.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$_SESSION['code']='123456';
+$code =$_SESSION['code'];
+include 'db.php';
+$phone = $_POST['phone'];
+$name = $_POST['name'];
+$id = $_POST['id'];
+$email = $_POST['email'];
+$sql = "select * 
+        from tb_user 
+        where name='$name'
+        and id='$id'
+        and phone='$phone'
+         or email ='$email'"; 
+$result = mysql_query($sql);
+$count = mysql_num_rows($result);
+
+if($count != 0 ){
+    echo $code;   
+}else{
+    echo "일치하는 정보가 존재하지않습니다"; 
+} 
+
+?>
